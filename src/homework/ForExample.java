c03524b39f11788cba13d7d14c1a068324c96122
sargis egoryan <sargisegoryan93@gmail.com>
2022-12-16 01:35:31 +0400

    do some changes

--- src/homework/ForExample.java
+++ src/homework/ForExample.java
@@ -2,8 +2,20 @@ package homework;
 
 public class ForExample {
     public static void main(String[] args) {
-        System.out.println("For Example");
 
-        System.out.println("some changes");
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println("kuku" + i);
+//        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
     }
+
+
 }
+
