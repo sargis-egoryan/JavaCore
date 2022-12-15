72d2a9120094d489c8d3c1acaab3f7c5881244f8
sargis egoryan <sargisegoryan93@gmail.com>
2022-12-16 00:15:32 +0400

    do some changes

--- src/homework/ForExample.java
+++ src/homework/ForExample.java
@@ -3,5 +3,7 @@ package homework;
 public class ForExample {
     public static void main(String[] args) {
         System.out.println("For Example");
+
+        System.out.println("some changes");
     }
 }
---
+++ src/homework/IfExample.java
@@ -0,0 +1,7 @@
+package homework;
+
+public class IfExample {
+    public static void main(String[] args) {
+        System.out.println("if example");
+    }
+}
