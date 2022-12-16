030b1af3611be55fa600fc054cd4f527f44ef183
sargis egoryan <sargisegoryan93@gmail.com>
2022-12-16 11:57:44 +0400

    do some changes

--- src/homework/ForExample.java
+++ src/homework/ForExample.java
@@ -6,8 +6,8 @@ public class ForExample {
 //        for (int i = 1; i <= 10; i++) {
 //            System.out.println("kuku" + i);
 //        }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 0; i <5; i++) {
+            for (int j = 0; j <=i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
