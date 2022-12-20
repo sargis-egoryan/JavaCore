012f9b60f8af76b9ac2097dfc5af1299f17f8b64
sargis egoryan <sargisegoryan93@gmail.com>
2022-12-20 11:54:26 +0400

    do some changes

--- src/homework/homework1/FigurePainter.java
+++ src/homework/homework1/FigurePainter.java
@@ -2,30 +2,30 @@ package homework.homework1;
 
 public class FigurePainter {
     public static void main(String[] args) {
-//        for (int i = 0; i <= 5; i++) {
-//            for (int j = 0; j < i; j++)
-//                System.out.print("* ");
-//            System.out.println();
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i; j++)
+                System.out.print("* ");
+            System.out.println();
 
 
-//        for (int i = 0; i < 5; i++) {
-//            for (int j = 0; j < 5; j++) {
-//                if (j < 5 - (1 + i)) {
-//                    System.out.print(" ");
-//                } else {
-//                    System.out.print("*");
-//                }
-//            }
-//            System.out.println();
-//        }
+        for ( i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j < 5 - (1 + i)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
 
 
-//        }
-//        for (int i = 0; i <= 5; i++) {
-//            for (int j = 5; j > i; j--)
-//                System.out.print("* ");
-//            System.out.println(" ");
-//        }
+    }
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 5; j > i; j--)
+                System.out.print("* ");
+            System.out.println(" ");
+        }
 
     }
 }
--- src/homework/homework1/IfForExample.java
+++ src/homework/homework1/IfForExample.java
@@ -3,33 +3,35 @@ package homework.homework1;
 public class IfForExample {
     public static void main(String[] args) {
 
-//        int a, b;
-//        a = 20;
-//        b = 40;
-//        if (a >= b) System.out.println(a);
-//        else
-//            System.out.println(b);
+        int a, b;
+        a = 20;
+        b = 40;
+        if (a >= b) System.out.println(a);
+        else
+            System.out.println(b);
 
-//        for (int i = 5; i <=10; i++) {
-//            System.out.print(i + " ");
+        for (int i = 5; i <= 10; i++) {
+            System.out.print(i + " ");
 
-//        int a = 65;
-//        char ch1 = (char) a;
-//        System.out.println(ch1);
-//        int b = 40;
-//        char ch2 = (char) b;
-//        System.out.println(ch2);
+//            int a = 65;
+            char ch1 = (char) a;
+            System.out.println(ch1);
+//            int b = 40;
+            char ch2 = (char) b;
+            System.out.println(ch2);
 
-//        int a, b;
-//        a = 20;
-//        b = 40;
-//        if (a == b) System.out.println("a=b");
-//        else
-//            System.out.println("a-ն հավասար չէ բ-ին");
 
+//            int a, b;
+            a = 20;
+            b = 40;
+            if (a == b) System.out.println("a=b");
+            else
+                System.out.println("a-ն հավասար չէ բ-ին");
+
+
+        }
 
     }
-
 }
 
 
