9911d6a2bd370b049604cdcdaec951053f06ede7
sargis egoryan <sargisegoryan93@gmail.com>
2023-01-21 20:44:52 +0400

    do some changes

--- src/homework/homework8/DynamicArray.java
+++ src/homework/homework8/DynamicArray.java
@@ -5,12 +5,28 @@ public class DynamicArray {
     private int size = 0;
 
     public void add(int value) {
-        if (size == 9) {
-            System.out.println("заполнен");
-        } else {
-            array[++size] = value;
+        if (size == array.length) {
+            extend();
+        }
+        array[++size] = value;
+    }
+
+    private void extend() {
+        int[] result = new int[array.length + 10];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        array = result;
+    }
+
+    public int getByIndex(int index) {
+        return index < 0 || index > size ? -1 : array[index];
+    }
+
+    public void print() {
+        for (int j : array) {
+            System.out.println(j);
         }
     }
-    int result[]=new int[15];
-    int index=0;
 }
+
--- src/homework/homework8/DynamicArrayDemo.java
+++ src/homework/homework8/DynamicArrayDemo.java
@@ -2,6 +2,9 @@ package homework.homework8;
 
 public class DynamicArrayDemo {
     public static void main(String[] args) {
-
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.add(1);
+        dynamicArray.add(2);
+        dynamicArray.add(3);
     }
 }
